f73aebb79f9004f99ee9a1cbd5d18ace182c6852
RobertoContEz <r.c0ntr3ras@gmail.com>
2025-09-04 21:52:28 -0700

    Avance en clase

---
+++ app/src/main/java/contreras/roberto/conversor/MainActivity.kt
@@ -0,0 +1,32 @@
+package contreras.roberto.conversor
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    var op : Int = 0 //0 longitud, 1 peso, 2 temp, 3 vol
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        cambiarOperacion()
+        
+    }
+
+    fun cambiarOperacion() {
+        when (op) {
+            0 ->
+        }
+    }
+}
